ee284392566287d28a9facaef71a33a3b70b86cc
Igor Davydenko <playpauseandstop@gmail.com>
2012-04-21 12:12:31 +0300

    Add ``_`` dummy function to the templates if no ``Flask-Babel`` installed.

--- flask_dropbox/blueprint.py
+++ flask_dropbox/blueprint.py
@@ -1,5 +1,12 @@
 from flask import Blueprint
 
+try:
+    from flask.ext.babel import Babel
+except ImportError:
+    BABEL_SUPPORTED = False
+else:
+    BABEL_SUPPORTED = True
+
 from .views import callback, logout
 
 
@@ -16,7 +23,8 @@ class DropboxBlueprint(Blueprint):
         """
         # Initialize blueprint instance
         defaults = {'name': 'dropbox',
-                    'import_name': __name__}
+                    'import_name': __name__,
+                    'template_folder': 'templates'}
         defaults.update(kwargs)
 
         super(DropboxBlueprint, self).__init__(**defaults)
@@ -27,3 +35,8 @@ class DropboxBlueprint(Blueprint):
 
         for url, view_func in url_map.items():
             self.add_url_rule(url, defaults=url_defaults, view_func=view_func)
+
+        if not BABEL_SUPPORTED:
+            @self.app_context_processor
+            def inject_underscore():
+                return {'_': lambda s: s}
