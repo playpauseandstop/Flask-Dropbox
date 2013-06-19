87c8ed9c168dacf22482407087e5e1f59d96dbeb
Igor Davydenko <playpauseandstop@gmail.com>
2013-06-19 14:39:49 +0300

    Bump version number.

--- flask_dropbox/__init__.py
+++ flask_dropbox/__init__.py
@@ -1,2 +1,6 @@
 from .blueprint import DropboxBlueprint
 from .extension import Dropbox
+
+__author__ = 'Igor Davydenko'
+__license__ = 'BSD License'
+__version__ = '0.3'
--- setup.py
+++ setup.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python
 
 import os
+import re
 
 from distutils.core import setup
 
 
-DIRNAME = os.path.dirname(__file__)
+DIRNAME = os.path.abspath(os.path.dirname(__file__))
+rel = lambda *parts: os.path.abspath(os.path.join(DIRNAME, *parts))
 
-readme = open(os.path.join(DIRNAME, 'README.rst'), 'r')
-README = readme.read()
-readme.close()
+README = open(rel('README.rst')).read()
+INIT_PY = open(rel('flask_dropbox', '__init__.py')).read()
+VERSION = re.findall("__version__ = '([^']+)'", INIT_PY)[0]
 
 
 setup(
     name='Flask-Dropbox',
-    version='0.2',
+    version=VERSION,
     description='Dropbox Python SDK support for Flask applications.',
     long_description=README,
     author='Igor Davydenko',
     author_email='playpauseandstop@gmail.com',
     url='https://github.com/playpauseandstop/Flask-Dropbox',
     install_requires=[
-        'Flask',
+        'Flask>=0.8',
         'dropbox',
     ],
     package_data={
